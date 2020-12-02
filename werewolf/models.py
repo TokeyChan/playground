d6cdbd29e4dbc6ec94febf47784cdad104c09902
TokeyChan <bengames@icloud.com>
2020-12-02 19:55:00 +0100

    Added WerewolfGame and GameSetting to werewolf.models

--- werewolf/models.py
+++ werewolf/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+from hotel_manager.models import Room
+
+
+class GameSetting(models.Model):
+    setting = models.ForeignKey("Setting", on_delete=models.CASCADE)
+    game = models.ForeignKey("WerewolfGame", on_delete=models.CASCADE)
+    value = models.CharField(max_length=25)
+
+
 class Setting(models.Model):
     name = models.CharField(max_length=25)
     type = models.CharField(max_length=15)
-    value = models.CharField(max_length=25, null=True, blank=True)
+    default_value = models.CharField(max_length=25, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class WerewolfGame(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
